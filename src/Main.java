a4b018e72671ced1902785a5fba48ca055a87533
PC <ilgizalmazov011@gmail.com>
2023-10-02 20:17:57 +0600

    первый коммит 22

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("task1");
     }
 }
